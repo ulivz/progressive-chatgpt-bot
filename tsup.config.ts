045cb036e62da8eff73a91039e2c46de22db33c9
chenhaoli <chenhaoli@bytedance.com>
2023-02-19 03:50:35 +0800

    fix: Error: ENOENT: no such file or directory, open '/code/node_modules/progressive-chatgpt-bot/dist/encoder.json'

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,3 +1,5 @@
+import { join } from 'path';
+import { copyFile } from 'fs/promises';
 import { defineConfig } from 'tsup';
 
 export default defineConfig({
@@ -10,4 +12,11 @@ export default defineConfig({
     'lodash.throttle',
     '@larksuiteoapi/node-sdk',
   ],
+  async onSuccess() {
+    const gpt3encoderDir = join(__dirname, 'node_modules/gpt-3-encoder');
+    const encoderJson = join(gpt3encoderDir, 'encoder.json');
+    const vocabBpe = join(gpt3encoderDir, 'vocab.bpe');
+    await copyFile(encoderJson, join(__dirname, 'dist/encoder.json'));
+    await copyFile(vocabBpe, join(__dirname, 'dist/vocab.bpe'));
+  },
 });
