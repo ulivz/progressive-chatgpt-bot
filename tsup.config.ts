9aa50875ab83d9d84d69e4c2e5f9d99da30d90f9
chenhaoli <chenhaoli@bytedance.com>
2023-02-19 03:57:46 +0800

    fix: chatgpt runtime usage

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,22 +1,13 @@
-import { join } from 'path';
-import { copyFile } from 'fs/promises';
 import { defineConfig } from 'tsup';
 
 export default defineConfig({
-  entry: ['src/index.ts'],
+  entry: ['esm/index.js'],
   splitting: false,
   sourcemap: true,
   clean: true,
   noExternal: [
-    'chatgpt',
     'lodash.throttle',
     '@larksuiteoapi/node-sdk',
   ],
-  async onSuccess() {
-    const gpt3encoderDir = join(__dirname, 'node_modules/gpt-3-encoder');
-    const encoderJson = join(gpt3encoderDir, 'encoder.json');
-    const vocabBpe = join(gpt3encoderDir, 'vocab.bpe');
-    await copyFile(encoderJson, join(__dirname, 'dist/encoder.json'));
-    await copyFile(vocabBpe, join(__dirname, 'dist/vocab.bpe'));
-  },
+  format: 'esm',
 });
